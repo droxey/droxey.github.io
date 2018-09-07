23b2f4d57dc0f2b645c170561d382e05ad67ac46
Dani Roxberry <dani@bitoriented.com>
2018-09-07 02:59:45 -0700

    [add] new cover page stuff

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm static/css/site.min.css
 echo "[BUILD] Removed old minified CSS and JS."
 
 echo "[BUILD] JS minifying..."
-uglifyjs --compress -o static/js/site.min.js -- static/js/jquery.address-1.5.min.js static/js/antiscroll.js static/js/jquery.fittext.js static/js/jquery.imagesloaded.min.js static/js/jquery.validate.min.js static/js/moment.min.js static/js/twitterFetcher_v10_min.js static/js/main.js static/js/custom.js
+uglifyjs --compress -o static/js/site.min.js -- static/js/jquery.address-1.5.min.js static/js/antiscroll.js static/js/jquery.fittext.js static/js/jquery.imagesloaded.min.js static/js/jquery.validate.min.js static/js/moment.min.js static/js/main.js static/js/custom.js
 echo "[BUILD] JS minified."
 
 echo "[BUILD] CSS minifying..."
